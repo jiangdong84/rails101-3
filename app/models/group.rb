b6223a3360cc12cefa815b88bd7e8a29abcdad60
jiangdong <jiangdong84@163.com>
2017-01-04 12:58:54 +0800

    df

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,10 @@
 class Group < ApplicationRecord
+  belongs_to :user
+  has_many :posts
+ validates :title, presence: true
+ has_many :group_relatioships
+ has_many :members, through: :group_relatioships, source: :user
+# has_many :group_relationships
+#   has_many :members, through: :group_relationships, source: :user
+
 end
